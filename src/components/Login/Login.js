1f7e6668dc32f30ff3f4141122bd55e1fd7d59a5
Shidul Islam <sishahin093@gmail.com>
2023-01-24 18:32:39 +0600

    done sovle

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
 
     const onSubmit = (data) => {
-        data.preventDefault();
+        // data.preventDefault();
         const emailauth = data.email;
         const databody = {
             email: data.email,
